c825f9453982240281f16d7f4e335d7d7a5c9dab
arun222 <arun9440910740@gmail.com>
2018-10-22 09:36:03 +0530

    Update main_list.py

--- Assignment2/main_list.py
+++ Assignment2/main_list.py
@@ -4,8 +4,8 @@ from list_node import *
 
 
 class main_list():
-    def __init__(self, head=None):
-        self.head = list_node(head)
+    def __init__(self, head=list_node()):
+        self.head = head
 
     def insert(new_list_node):
         temp = self.head
