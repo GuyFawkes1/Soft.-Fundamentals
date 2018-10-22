4c033b6bd325f3719fa5651d59d4f4ae3b63b0c0
arun222 <arun9440910740@gmail.com>
2018-10-22 16:39:07 +0530

    Update graph.py

--- Assignment2/graph.py
+++ Assignment2/graph.py
@@ -5,14 +5,14 @@ class graph():
     def __init__(self, vertices, cost_matrix):
         self.vertices = vertices
         self.cost_matrix = cost_matrix
-        self.adj = [None]*len(vertices)
+        self.adj = [collections.deque([])]*len(vertices)
 
-    def has_edge(self,vertex1, vertex2):
+    def has_edge(self, vertex1, vertex2):
         if self.cost_matrix[vertex1-1][vertex2-1] > 0:
             return True
         return False
 
-    def add_edge(self,vertex1, vertex2):
+    def add_edge(self, vertex1, vertex2):
         # self.cost_matrix[vertex1-1][vertex2-1]=self.cost_matrix[vertex1-1][vertex2-1]+weight
         temp1 = self.adj[vertex1-1]
         if vertex2 not in temp1:
